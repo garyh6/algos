66e2bfa31fb9993b2fa2fd1b5c5e9d5e10f42acb
gary <garyh6@users.noreply.github.com>
2020-11-01 00:13:14 -0700

    easy

---
+++ 1431-KidsWiththeGreatestNumberofCandies.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    const max = Math.max(...candies)
+    const res = []
+    for(let i=0; i<candies.length; i++) {
+        if(candies[i] + extraCandies >= max) res.push(true)
+        else res.push(false)
+    }
+    
+    return res
+};
